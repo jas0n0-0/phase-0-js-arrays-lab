e3a4b78d039a748c17d27550f0737744ba5999ef
jason_mumo <jason.mumo@student.moringaschool.com>
2026-03-24 10:49:23 +0300

    creation of product array with relevant functions for manipulation

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,8 +1,20 @@
-// Write your code here
+let products = ["Laptop","Phone","Headphones","Monitor"]
+function logFirstProduct(){
+  console.log(products[0])
+}
+function addProduct(newProduct){
+  products.push(newProduct)
+}
+function updateProductName(productPosition,newName){
+  products[productPosition] = newName
+}
+function removeLastProduct(){
+  products.pop()
+}
 
 
 
-// Export the necessary parts for testing
+
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
